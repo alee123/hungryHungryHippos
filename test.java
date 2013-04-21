6bd4f91d0bfef415bc95e9500660e534a8832177
skinc <shane.skikne@students.olin.edu>
2013-04-20 20:15:14 -0400

    Test from shane

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("If this works, shane will be really really happy!");
     }
 
 }
