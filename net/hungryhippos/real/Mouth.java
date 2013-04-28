ba1d8627fd493628ba00fedb197b4d8230119528
Rachel Boy <rachel.boy@gmail.com>
2013-04-28 18:19:13 -0400

    mouth class

---
+++ net/hungryhippos/real/Mouth.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package net.hungryhippos.real;
+
+import com.jme3.math.Vector3f;
+
+/**Tracks the position and attributes of the players mouth
+ * @author rboy, vcoleman
+ *
+ */
+public class Mouth {
+	//x, y, and z should be values between 0 and 1
+	private float x;
+	private float y;
+	private float z;
+	private boolean open;
+	private boolean calibrated;
+
+	/**
+	 * 
+	 */
+	public Mouth(float x, float y, float z) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setOpen(boolean open){
+		this.open = open;
+	}
+	
+	public void setCalibrated(boolean calibrated){
+		this.calibrated = calibrated;
+	}
+
+	public void setX(int x) {
+		if (x>=0 && x<=1){
+			this.x = x;
+		}
+		else if (x>1){
+			this.x = 1;
+		}
+		else if (x<0){
+			this.x = 0;
+		}
+	}
+
+	public void setY(int y) {
+		if (y>=0 && y<=1){
+			this.y = y;
+		}
+		else if (y>1){
+			this.y = 1;
+		}
+		else if (y<0){
+			this.y = 0;
+		}
+	}
+
+	public void setZ(int z) {
+		if (z>=0 && z<=1){
+			this.z = z;
+		}
+		else if (z>1){
+			this.z = 1;
+		}
+		else if (z<0){
+			this.z = 0;
+		}
+	}
+
+	public boolean isOpen() {
+		return open;
+	}
+
+	public boolean isCalibrated() {
+		return calibrated;
+	}
+	
+	public Vector3f getPosition() {
+		return new Vector3f(x,y,z);
+	}
+
+}
